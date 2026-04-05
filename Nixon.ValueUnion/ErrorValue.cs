c71540d63afb6f3a69543cd973a40e86142b2fc7
Joshua Nixon <nixonjoshua98@gmail.com>
2026-04-05 16:27:20 +0100

    Added ValueUnion

---
+++ Nixon.ValueUnion/ErrorValue.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nixon.ValueUnion;
+
+public enum ErrorType : byte
+{
+    NotFound,
+    ClientError,
+    Forbidden
+}
+
+public sealed record ErrorValue(ErrorType ErrorType, string? Message)
+{
+    private readonly Dictionary<string, object?> _additionalValues = [];
+
+    public ErrorValue WithValue(string key, object data)
+    {
+        _additionalValues[key] = data;
+        return this;
+    }
+
+    public static ErrorValue NotFoundError(string? message = null)
+    {
+        return new ErrorValue(ErrorType.NotFound, message);
+    }
+
+    public static ErrorValue ClientError(string? message = null)
+    {
+        return new ErrorValue(ErrorType.ClientError, message);
+    }
+
+    public static ErrorValue Forbidden(string? message = null)
+    {
+        return new ErrorValue(ErrorType.Forbidden, message);
+    }
+    
+    internal ProblemDetails ToProblemDetails()
+    {
+        return new ProblemDetails
+        {
+            Detail = Message,
+            Extensions = _additionalValues,
+            Status = ErrorType switch
+            {
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.ClientError => StatusCodes.Status400BadRequest,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                _ => StatusCodes.Status500InternalServerError
+            }
+        };
+    }
+    
+
+}
---
+++ Nixon.ValueUnion/ValueOr.cs
@@ -0,0 +1,60 @@
+﻿namespace Nixon.ValueUnion
+{
+    public class ValueOr<T, TOther>
+    {
+        private readonly T _value;
+
+        private readonly TOther _other;
+
+        public T Value => 
+            HasValue ? 
+                _value : 
+                throw new InvalidOperationException("Attempted to access a non-specified value");
+
+        public bool HasValue { get; }
+
+        protected ValueOr(T value)
+        {
+            HasValue = true;
+
+            _value = value;
+            _other = default!;
+        }
+
+        protected ValueOr(TOther problem)
+        {
+            _other = problem;
+            _value = default!;
+        }
+
+        public bool TryGetValue(out T value, out TOther other)
+        {
+            value = _value;
+            other = _other;
+
+            return HasValue;
+        }
+
+        public static ValueOr<T, TOther> From(T value)
+        {
+            return new ValueOr<T, TOther>(value);
+        }
+
+        public static ValueOr<T, TOther> FromOther(TOther value)
+        {
+            return new ValueOr<T, TOther>(value);
+        }
+
+
+        public static implicit operator ValueOr<T, TOther>(T value)
+        {
+            return new ValueOr<T, TOther>(value);
+        }
+
+
+        public static implicit operator ValueOr<T, TOther>(TOther problem)
+        {
+            return new ValueOr<T, TOther>(problem);
+        }
+    }
+}
