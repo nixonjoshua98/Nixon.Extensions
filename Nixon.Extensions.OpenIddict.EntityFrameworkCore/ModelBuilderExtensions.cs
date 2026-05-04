5e736fac70ad1442e97f8fd2b92945649787d2ee
Joshua Nixon <nixonjoshua98@gmail.com>
2026-05-04 15:51:00 +0100

    Updated extensions for openiddict

--- Nixon.Extensions.OpenIddict.EntityFrameworkCore/ModelBuilderExtensions.cs
+++ Nixon.Extensions.OpenIddict.EntityFrameworkCore/ModelBuilderExtensions.cs
@@ -1,29 +1,108 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using OpenIddict.EntityFrameworkCore.Models;
+
+// ReSharper disable InvertIf
 
 namespace Nixon.Extensions.OpenIddict.EntityFrameworkCore;
 
 public static class ModelBuilderExtensions
 {
-    public static void ConfigureDefaultOpenIddictModels(
-        this ModelBuilder modelBuilder, 
-        Action<IMutableEntityType> configure)
+    public static void UseOpenIddict(
+        this ModelBuilder modelBuilder,
+        string schemaName,
+        string applicationTableName,
+        string authorizationTableName,
+        string scopeTableName,
+        string tokenTableName)
     {
-        var modelAssembly = typeof(OpenIddictEntityFrameworkCoreApplication).Assembly;
+        modelBuilder.UseOpenIddict(
+            application =>
+            {
+                application.SetSchema(schemaName);
+                application.SetTableName(applicationTableName);
+            },
+            authorization =>
+            {
+                authorization.SetSchema(schemaName);
+                authorization.SetTableName(authorizationTableName); 
+            },
+            scope =>
+            {
+                scope.SetSchema(schemaName);
+                scope.SetTableName(scopeTableName);
+            },
+            token =>
+            {
+                token.SetSchema(schemaName);
+                token.SetTableName(tokenTableName);
+            }
+        );
+    }
+
+    public static void UseOpenIddict(
+        this ModelBuilder modelBuilder,
+        Action<IMutableEntityType>? configureApplication = null,
+        Action<IMutableEntityType>? configureAuthorization = null,
+        Action<IMutableEntityType>? configureScope = null,
+        Action<IMutableEntityType>? configureToken = null)
+    {
+        modelBuilder.UseOpenIddict<
+            OpenIddictEntityFrameworkCoreApplication,
+            OpenIddictEntityFrameworkCoreAuthorization,
+            OpenIddictEntityFrameworkCoreScope,
+            OpenIddictEntityFrameworkCoreToken,
+            string
+        >(
+            configureApplication,
+            configureAuthorization,
+            configureScope,
+            configureToken
+        );
+    }
+    
+    public static void UseOpenIddict<TApplication, TAuthorization, TScope, TToken, TKey>(
+        this ModelBuilder modelBuilder,
+        Action<IMutableEntityType>? configureApplication = null,
+        Action<IMutableEntityType>? configureAuthorization = null,
+        Action<IMutableEntityType>? configureScope = null,
+        Action<IMutableEntityType>? configureToken = null)
+        where TApplication : OpenIddictEntityFrameworkCoreApplication<TKey, TAuthorization, TToken>
+        where TAuthorization : OpenIddictEntityFrameworkCoreAuthorization<TKey, TApplication, TToken>
+        where TScope : OpenIddictEntityFrameworkCoreScope<TKey>
+        where TToken : OpenIddictEntityFrameworkCoreToken<TKey, TApplication, TAuthorization>
+        where TKey : IEquatable<TKey>
+    {
+        modelBuilder.UseOpenIddict();
         
         foreach (var entity in modelBuilder.Model.GetEntityTypes())
         {
             for (var type = entity; type != null; type = type.BaseType)
             {
-                if (type.ClrType.Assembly != modelAssembly)
+                if (type.ClrType.Assembly == typeof(TApplication).Assembly)
                 {
-                    continue;
+                    configureApplication?.Invoke(type);
+                    break;
                 }
                 
-                configure(type);
-
-                break;
+                if (type.ClrType.Assembly == typeof(TAuthorization).Assembly)
+                {
+                    configureAuthorization?.Invoke(type);
+                    break;
+                }
+                
+                if (type.ClrType.Assembly == typeof(TScope).Assembly)
+                {
+                    configureScope?.Invoke(type);
+                    break;
+                }
+                
+                if (type.ClrType.Assembly == typeof(TToken).Assembly)
+                {
+                    configureToken?.Invoke(type);
+                    break;
+                }
             }
         }
     }
