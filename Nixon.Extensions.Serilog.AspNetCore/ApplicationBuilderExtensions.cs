b17caf2b2dd5be873310cdb476a0c4846fcdcb35
Joshua Nixon <nixonjoshua98@gmail.com>
2026-05-18 19:57:42 +0100

    Corrected a typo within the serilog extensions

--- Nixon.Extensions.Serilog.AspNetCore/ApplicationBuilderExtensions.cs
+++ Nixon.Extensions.Serilog.AspNetCore/ApplicationBuilderExtensions.cs
@@ -9,7 +9,7 @@ namespace Nixon.Extensions.Serilog.AspNetCore;
 
 public static class ApplicationBuilderExtensions
 {
-    public static T UserSerilogRequestLoggingWithAdditionalOptions<T>(
+    public static T UseAdditionalSerilogRequestLogging<T>(
         this T app, 
         Action<AdditionalSerilogRequestLoggingOptions>? configure = null) 
         where T : IApplicationBuilder
